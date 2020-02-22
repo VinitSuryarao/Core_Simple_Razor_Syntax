295ef626d17916ed14e4ba4459adc54506200cd9
Vinit Suryarao <60574327+VinitSuryarao@users.noreply.github.com>
2020-02-22 20:01:52 +0530

    Add files via upload

---
+++ Simple_Razor_Syntax/Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Simple_Razor_Syntax.Models
+{
+    public class UserModel
+    {
+
+        public String FirstName { get; set; }
+
+        public String LastName { get; set; }
+    }
+}
